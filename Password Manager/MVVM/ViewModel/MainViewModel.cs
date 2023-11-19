7e800dbf9cbfe5b51cd55d41d9966b228928f2be
WayneMt <nyashamtemeregwa@gmail.com>
2023-11-19 23:44:04 +0000

    first commit

---
+++ Password Manager/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Password_Manager.Core;
+using System;
+
+namespace Password_Manager.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand MyVaultViewCommand { get; set; }
+        public RelayCommand FavouritesViewCommand { get; set; }
+        public RelayCommand PaymentsViewCommand { get; set; }
+        public RelayCommand PasswordGeneratorViewCommand { get; set; }
+
+        public MyVaultViewModel MyVaultVM { get; set; }
+        public FavouritesViewModel FavouritesVM { get; set; }
+        public PaymentsViewModel PaymentsVM { get; set; }
+        public PasswordGeneratorViewModel PasswordGeneratorVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() 
+        { 
+            MyVaultVM = new MyVaultViewModel();
+            FavouritesVM = new FavouritesViewModel();
+            PaymentsVM = new PaymentsViewModel();
+            PasswordGeneratorVM = new PasswordGeneratorViewModel();
+            CurrentView = MyVaultVM;
+
+            MyVaultViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = MyVaultVM;
+            });
+
+            FavouritesViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = FavouritesVM;
+            });
+
+            PaymentsViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = PaymentsVM;
+            });
+
+            PasswordGeneratorViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = PasswordGeneratorVM;
+            });
+        }
+    }
+}
---
+++ Password Manager/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Password_Manager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
